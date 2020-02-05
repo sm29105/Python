aed8d29e4054efdfd38e169ac9f0ad2db3eb1603
Ines Mahjoub <ines.mahjoub@draexlmaier.com>
2020-02-05 14:23:59 +0100

    My first change

--- hanoi.py
+++ hanoi.py
@@ -1,9 +1,9 @@
 def hanoi(frm, tmp, target, count):
     if count == 1:
-        print ("Move from '" + frm + "' to '" + target + "'")
+        print ("Move tile from '" + frm + "' to '" + target + "'")
     else:
         hanoi(frm, target, tmp, count - 1)
-        print ("Move from '" + frm + "' to '" + target + "'")
+        print ("Move tile from '" + frm + "' to '" + target + "'")
         hanoi(tmp, frm, target, count - 1)
 
 hanoi("Left Pile", "Middle Pile", "Right Pile", 8)
